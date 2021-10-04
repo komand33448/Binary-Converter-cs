4d99d5f10e81bcae41613d550eff5f9e97bad416
komand33448 <90390242+komand33448@users.noreply.github.com>
2021-10-04 23:58:37 +0400

    Add files via upload

---
+++ binaryIDE2/binaryIDE2.0/Form2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace binaryIDE2._0
+{
+    public partial class two : Form
+    {
+        public two()
+        {
+            InitializeComponent();
+        }
+        public void inputTaker2_TextChanged(object sender, EventArgs e)
+        {
+            string input = inputTaker2.Text;
+            //int input;
+            //string finalInput;
+
+            //int.TryParse(rawInput, out input);
+            Convert.ToInt64("input", 2).ToString();
+            //finalInput = input.ToString();
+            
+            ans2.Text = input;
+        }
+        private void darkButton2_Click(object sender, EventArgs e)
+        {
+            sike2.Text = "Lol you wish";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            one c = new one();
+            this.Hide();
+            c.ShowDialog();
+            this.Close();
+        }
+        //useless shit
+        private void sike2_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private void two_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void ans2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
