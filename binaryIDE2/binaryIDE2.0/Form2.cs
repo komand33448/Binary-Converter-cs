4cff86e304d9aee941c21f943ee24f285d141167
komand33448 <90390242+komand33448@users.noreply.github.com>
2021-10-05 18:32:15 +0400

    Update Form2.cs

--- binaryIDE2/binaryIDE2.0/Form2.cs
+++ binaryIDE2/binaryIDE2.0/Form2.cs
@@ -16,18 +16,20 @@ namespace binaryIDE2._0
         }
         public void inputTaker2_TextChanged(object sender, EventArgs e)
         {
+            //det var mening at den her del skulle lave binary til decimal
+            //men har nu valgt at lægge den på hyllen for nu
             string input = inputTaker2.Text;
             Int64 test2 = Convert.ToInt64(input);  
             
             ans2.Text = test2.ToString();
         }
         private void darkButton2_Click(object sender, EventArgs e)
-        {
+        {            
             sike2.Text = "Lol you wish";
         }
 
         private void button12_Click(object sender, EventArgs e)
-        {
+        {            
             one c = new one();
             this.Hide();
             c.ShowDialog();
